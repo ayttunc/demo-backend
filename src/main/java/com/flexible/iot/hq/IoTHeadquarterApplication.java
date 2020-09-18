f5ce81c6e5a462994a93727464ada333c417e7bc
Aytunc matrac <aytunc.matrac@partner.turktelekom.com.tr>
2020-09-19 01:30:16 +0300

    encryption

--- src/main/java/com/flexible/iot/hq/IoTHeadquarterApplication.java
+++ src/main/java/com/flexible/iot/hq/IoTHeadquarterApplication.java
@@ -4,6 +4,7 @@ import com.flexible.iot.hq.config.FileStorageConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableConfigurationProperties({
@@ -14,5 +15,10 @@ public class IoTHeadquarterApplication {
     public static void main(String[] args) {
         SpringApplication.run(IoTHeadquarterApplication.class, args);
     }
+
+//    @Bean
+//    public BCryptPasswordEncoder bCryptPasswordEncoder(){
+//        return new BCryptPasswordEncoder();
+//    }
 
 }
--- src/main/java/com/flexible/iot/hq/service/impl/UserServiceImpl.java
+++ src/main/java/com/flexible/iot/hq/service/impl/UserServiceImpl.java
@@ -3,6 +3,7 @@ package com.flexible.iot.hq.service.impl;
 import com.flexible.iot.hq.io.entity.UserEntity;
 import com.flexible.iot.hq.io.repository.UserRepository;
 import com.flexible.iot.hq.service.UserService;
+import com.flexible.iot.hq.shared.Utils;
 import com.flexible.iot.hq.shared.dto.UserDto;
 import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
@@ -11,19 +12,25 @@ import org.springframework.stereotype.Service;
 public class UserServiceImpl implements UserService {
 
     private final UserRepository userRepository;
+    private final Utils utils;
 
-    public UserServiceImpl(UserRepository userRepository) {
+    public UserServiceImpl(UserRepository userRepository, Utils utils) {
         this.userRepository = userRepository;
+        this.utils = utils;
     }
 
     @Override
     public UserDto createUser(UserDto user) {
 
+        if(userRepository.findByEmail(user.getEmail()) != null){
+            throw new RuntimeException("Record Already exist");
+        }
+
         UserEntity userEntity = new UserEntity();
         BeanUtils.copyProperties(user, userEntity);
 
+        userEntity.setUserId(utils.generateUserId(30));
         userEntity.setEncryptedPassword("Test");
-        userEntity.setUserId("Test UserId");
 
         UserEntity storedUserDetails = userRepository.save(userEntity);
 
